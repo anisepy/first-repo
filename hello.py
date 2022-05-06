ffb5a3bae519c933527a3092c1ae91cfc198e7c8
anisepy <heesu940@gmail.com>
2022-05-06 13:33:35 +0900

    test3

--- hello.py
+++ hello.py
@@ -5,12 +5,12 @@
 print('hello')
 
 for i in range(1,21) :
-	if i%3 == 0 :
+	if i%15 == 0 :
+		print("yeardream")
+	elif i%3 == 0 :
 		print("year")
 	elif i%5 == 0 :
 		print("dream")
-	elif (i%3==0) and (i%5 ==0) :
-		print("yeardream")
 	else :
 		print(i)
 
