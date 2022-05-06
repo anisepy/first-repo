989949c4eddfc3ba009c7b770f355c0654e2d041
anisepy <heesu940@gmail.com>
2022-05-06 13:25:01 +0900

    test2

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 #과제 : 1부터 10까지 반복하는 과정에서 3의 배수일 걍우, year를 출력하시오.
+#5의 배수일때 dream출력
+# 15의 배수일 때 yeardream 출력
 #나머지  모든 경우는 숫자 그대로 출력
 print('hello')
 
-for i in range(1,11) :
+for i in range(1,21) :
 	if i%3 == 0 :
 		print("year")
 	elif i%5 == 0 :
